a39983b235026d123a194e4a63b5d0fcae428235
Arpon <arpon.paul55@gmail.com>
2025-07-20 16:40:41 +0600

    LT!

---
+++ Mid/Lab Task 1/View/Task1.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<title>AIUB</title>
+
+<body>
+    <center style="color: blue;">
+        <h1>AIUB</h1>
+        <h2> Registration Form</h2>
+
+    </center>
+
+    <h3 align="left"> Complete the Registration</h1>
+
+        <table>
+            <tr>
+                <td>
+                    Full Name: </td>
+                <td><input type="text"></td>
+            </tr>
+
+            <tr>
+                <td>
+                    Email: </td>
+                <td><input type="text"></td>
+            </tr>
+
+             <tr>
+                <td>
+                    Password: </td>
+                <td><input type="text"></td>
+            </tr>
+
+             <tr>
+                <td>
+                    Gender:</td>
+
+                <td><input type="radio" name="Gender">Male</td>
+                <td><input type="radio" name="Gender">other</td>
+              
+            </tr>
+
+            <tr>
+                <td>
+                    Languages Known:</td>
+
+                <td><input type="radio" name="Language">English</td>
+                <td><input type="radio" name="Language">Bangla</td>
+                <td><input type="radio" name="Language">Hindi</td>
+            </tr>
+
+
+            <tr>
+              <td> Country: </td>
+              <td><select>
+                <option value = "">-- Select option 
+                  <option value = "">-- Bangladesh
+                    <option value = "">-- India
+                      <option value = "">-- UK
+                        <option value = "">-- USA
+                          <option value = "">-- Canada
+                            
+              </select></td>
+
+              <tr>
+                <td> Date of Birth</td>
+                <td><input type="date"></td>
+            </tr>
+
+
+
+            <tr>
+                <td> Upload Photo:</td>
+                <td><input type="file"></td>
+            </tr>
+
+
+            <tr>
+                <td> Comments:</td>
+                <td><textarea rows = "6" cols = "60" ></textarea></td>
+            </tr>
+
+
+
+
+
+
+
+            
+
+
+
+            
+
+            
+
+            
+
+            </tr>
+            
+
+
+
+
+        </table>
+
+        <br><br>
+        
+        <input type ="Submit" value="Register">
+        
+
+</body>
+
+</html>
